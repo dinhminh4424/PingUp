82bdf66ffc48cfba17ef360f781cd5c58ec5638d
DINHCONGMINH <dinhminh4424@gmail.com>
2026-06-28 15:34:59 +0700

    feat: implement user profile update functionality with image upload

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -34,6 +34,69 @@ class UserController {
       });
     }
   }
+
+  async updateInfoUser(req, res) {
+    try {
+      const user = req.user;
+      const { username, full_name, bio, location } = req.body;
+
+      // Dùng upload.fields() thì sẽ có dạng
+
+      // ============================  req.files; ============================
+
+      // req.files = {
+      //   profile_picture: [
+      //     {
+      //       fieldname: "profile_picture",
+      //       originalname: "avatar.jpg",
+      //       filename: "...",
+      //       ...
+      //     }
+      //   ],
+      //   cover_photo: [
+      //     {
+      //       fieldname: "cover_photo",
+      //       originalname: "cover.jpg",
+      //       filename: "...",
+      //       ...
+      //     }
+      //   ]
+      // }
+
+      console.log("req.files: ", req.files);
+
+      const profile_picture = req.files?.profile_picture?.[0];
+      const cover_photo = req.files?.cover_photo?.[0];
+
+      console.log("========= Update ==========");
+      console.log("user._id: ", user._id);
+      console.log("username: ", username);
+      console.log("full_name: ", full_name);
+      console.log("bio: ", bio);
+      console.log("location: ", location);
+      console.log("profile_picture: ", profile_picture);
+      console.log("cover_photo: ", cover_photo);
+
+      const result = await UserService.updateInfoUser(
+        user._id,
+        username,
+        full_name,
+        bio,
+        location,
+        profile_picture,
+        cover_photo,
+      );
+
+      res.status(result.status).json(result.data);
+    } catch (error) {
+      console.error("Lỗi khi chỉnh sửa thông tin user updateInfoUser:", error);
+      res.status(500).json({
+        success: false,
+        message:
+          "Lỗi khi chỉnh sửa thông tin user updateInfoUser:" + error.message,
+      });
+    }
+  }
 }
 
 export default new UserController();
--- backend/src/services/UserService.js
+++ backend/src/services/UserService.js
@@ -1,7 +1,7 @@
 import User from "../models/User.js";
 import { uploadImageFromBuffer } from "../middlewares/UpLoadMiddleware.js";
 
-class PostService {
+class UserService {
   async getUserById(id) {
     try {
       const user = await User.findOne({
@@ -26,6 +26,62 @@ class PostService {
       };
     }
   }
+
+  async updateInfoUser(
+    id,
+    username,
+    full_name,
+    bio,
+    location,
+    profile_picture,
+    cover_photo,
+  ) {
+    try {
+      const data = {};
+
+      if (username !== undefined) data.username = username;
+      if (full_name !== undefined) data.full_name = full_name;
+      if (bio !== undefined) data.bio = bio;
+      if (location !== undefined) data.location = location;
+
+      if (profile_picture) {
+        const uploadResult = await uploadImageFromBuffer(
+          profile_picture.buffer,
+        );
+
+        data.profile_picture = uploadResult.secure_url;
+      }
+
+      if (cover_photo) {
+        const uploadResult = await uploadImageFromBuffer(cover_photo.buffer);
+
+        data.cover_photo = uploadResult.secure_url;
+      }
+
+      const user = await User.findByIdAndUpdate(id, data, { returnDocument: "after" });
+
+      console.log("User: ", user);
+
+      return {
+        status: 200,
+        data: {
+          success: true,
+          message: `Cạp nhật thông tin user id: ${id}  thành công! `,
+          user: user,
+        },
+      };
+    } catch (error) {
+      console.log("Lỗi khi chỉnh sửa thông tin user updateInfoUser: ", error);
+      return {
+        status: 500,
+        data: {
+          success: false,
+          message:
+            "Lỗi khi chỉnh sửa thông tin user updateInfoUser: " + error.message,
+        },
+      };
+    }
+  }
 }
 
-export default new PostService();
+export default new UserService();
